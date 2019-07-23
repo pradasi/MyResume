f5b0ed4f2ad2198eb77c70729cd32c134659da11
pradeep <pradeepkumar2497@gmail.com>
2019-07-23 17:18:11 +0530

    Updates

---
+++ precache-manifest.2eca34127cfdb0bb121720c00f706985.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c87eed1861ff67b3f35d23a655ff51d8",
+    "url": "/MyResume/index.html"
+  },
+  {
+    "revision": "615fa7c6bb5e4c9e1ee2",
+    "url": "/MyResume/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "c969990ac65db5d400f7",
+    "url": "/MyResume/static/css/main.aad07c76.chunk.css"
+  },
+  {
+    "revision": "615fa7c6bb5e4c9e1ee2",
+    "url": "/MyResume/static/js/2.5778a1d2.chunk.js"
+  },
+  {
+    "revision": "c969990ac65db5d400f7",
+    "url": "/MyResume/static/js/main.6d4caf40.chunk.js"
+  },
+  {
+    "revision": "c777bb308bc8e9d8e4f8",
+    "url": "/MyResume/static/js/runtime~main.17fc0005.js"
+  },
+  {
+    "revision": "3c9690ff59139cf9f7976cf94bea3ea5",
+    "url": "/MyResume/static/media/Home.3c9690ff.jpg"
+  },
+  {
+    "revision": "3c9d08c275c1d75922e3c982b2471ddc",
+    "url": "/MyResume/static/media/cosmology-webfont.3c9d08c2.woff2"
+  },
+  {
+    "revision": "f9779d0e902c767da592790a47a92bbc",
+    "url": "/MyResume/static/media/cosmology-webfont.f9779d0e.woff"
+  },
+  {
+    "revision": "85eeeebf3d8f5d8688bd803876287753",
+    "url": "/MyResume/static/media/humanoid-webfont.85eeeebf.woff2"
+  },
+  {
+    "revision": "def23da37be1311d90ba213e1f7cf1e1",
+    "url": "/MyResume/static/media/humanoid-webfont.def23da3.woff"
+  },
+  {
+    "revision": "051ca7bc5c868117731388984b4d9577",
+    "url": "/MyResume/static/media/humanoidstraight-webfont.051ca7bc.woff"
+  },
+  {
+    "revision": "46f1afea7ddbfe88abd12983f09491c3",
+    "url": "/MyResume/static/media/humanoidstraight-webfont.46f1afea.woff2"
+  },
+  {
+    "revision": "373f259d7c18947d4448a348c187dd40",
+    "url": "/MyResume/static/media/img.373f259d.jpg"
+  },
+  {
+    "revision": "439240d566f56d296fe4a80f8c594730",
+    "url": "/MyResume/static/media/leftArrow.439240d5.png"
+  }
+]);
