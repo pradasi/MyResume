38380a0ac5740c7722283c418ec5b29f128b3f97
pradeep <pradeepkumar2497@gmail.com>
2019-07-27 19:19:49 +0530

    almost there

--- src/App.js
+++ src/App.js
@@ -45,9 +45,6 @@ class App extends Component{
 	}
 
 	componentDidMount(){
-		/*fetch('https://jsonplaceholder.typicode.com/users')
-		 .then(response => response.json())
-		 .then(users => this.setState({robots: users})  )*/
 		 this.setState({robots: robots})
 		
 	}
--- src/Components/CardList/Card.js
+++ src/Components/CardList/Card.js
@@ -6,7 +6,7 @@ const Card = ({name,email,id,click}) => {
 		<div className="dib back br2 pa2 ma2 grow shadow-5" onClick={ ()=> click(`${id}`)}>
 			<img alt=	{`robo${id}`} src={`https://robohash.org/${id}`} height={'200'} width={'200'} />
 			<div>
-			<h2 className='fnt' style={{color:'#f1f1f1',fontFamily:'inconsolataregular'}}>{name} </h2>	
+			<h3 style={{color:'#1F1F1F',fontFamily:'inconsolataregular'}}>{name} </h3>	
 			</div>
 		</div>
 		);
