2730c48f3f12f9ae3d5c3156c233e44ee39cafce
pradeep <pradeepkumar2497@gmail.com>
2019-08-01 13:22:30 +0530

    final

--- src/Components/Projects/content.js
+++ src/Components/Projects/content.js
@@ -17,8 +17,7 @@ const cont = [
 		Approach:"Develop a website where customers can book for different services",
 		Outcome: "Learned php, relational database and web development",
 		image: "Portal",
-		Abstract: "The aim of our project is to increase the Productivity of an NGO which operates from more than one office pan in India Obtaining global productivity for any task gets challenging and is a tedious exercise, for e.g. sorting of material to form a distributable kit involves number of people, making of a school bag from re-usable material involves different set of people with requisite skills sets.In order to improve productivity, we will make an ERP which will predict sales of the NGO for every month Accurate sales forecasts will enable NGO to make informed business decisions and predict short-term and long-term performance Also in our ERP, we will allow to share weekly Report into the database as well as graphical representation of sales which will help to assess the performance"
-
+		Abstract:"In order to provide easy, stay at home experience for users, our website: “ONLINE SERVICE PORTAL” is going to be very helpful, as it has an easy to use interface and quick response for any service that would be booked.Book for any number of services and and it will be processed as soon as possible as the website informs the vendors as soon as a customer is in need.",
 	},
 	{
 		id: 3,
@@ -28,7 +27,7 @@ const cont = [
 		Approach:"Build a website which shows the working of various Data Structures",
 		Outcome: "Learned few concepts in JavaScript",
 		image: "Virtual",
-		Abstract: "The aim of our project is to increase the Productivity of an NGO which operates from more than one office pan in India Obtaining global productivity for any task gets challenging and is a tedious exercise, for e.g. sorting of material to form a distributable kit involves number of people, making of a school bag from re-usable material involves different set of people with requisite skills sets.In order to improve productivity, we will make an ERP which will predict sales of the NGO for every month Accurate sales forecasts will enable NGO to make informed business decisions and predict short-term and long-term performance Also in our ERP, we will allow to share weekly Report into the database as well as graphical representation of sales which will help to assess the performance"
+		Abstract: "Practical experience is an important part of the educational process. However, the time and economical resources often required for the setting up and construction of scientific laboratories is outside the scope of many institutions. A solution to this problem could be found in the adaptation of the Virtual Laboratories which will simulate the processes and actions that could take place in real laboratories."
 	},
 	{
 		id: 4,
@@ -38,18 +37,9 @@ const cont = [
 		Approach:"Generate an temporary OTP and communicates through client server architechture",
 		Outcome: "Learned the rules in generating OTP and several related RFCs'",
 		image: "OTP",
-		Abstract: "The aim of our project is to increase the Productivity of an NGO which operates from more than one office pan in India Obtaining global productivity for any task gets challenging and is a tedious exercise, for e.g. sorting of material to form a distributable kit involves number of people, making of a school bag from re-usable material involves different set of people with requisite skills sets.In order to improve productivity, we will make an ERP which will predict sales of the NGO for every month Accurate sales forecasts will enable NGO to make informed business decisions and predict short-term and long-term performance Also in our ERP, we will allow to share weekly Report into the database as well as graphical representation of sales which will help to assess the performance"
+		Abstract: "One-time password or OTP is a password that is applicable for only one login session or transaction, on a computer system or different digital device. OTPs ignore various shortcomings that are linked with traditional, i.e., static password-based authentication; a number of accomplishments also integrates two-factor authentication by making sure that one-time password needs access to something a person has plus something a person already knows."
 	},
-	{
-		id: 5,
-		title: "Bengaluru Awake",
-		duration: "Sept 2018 (1-day Hackathon Project) ",
-		Aim: "An Awareness Page",
-		Approach:"Designing a web page to show pollution and traffic problems in Banglore",
-		Outcome: "Experiencing the thrill of one day hackathon",
-		image: "Dashboard",
-		Abstract: "The aim of our project is to increase the Productivity of an NGO which operates from more than one office pan in India Obtaining global productivity for any task gets challenging and is a tedious exercise, for e.g. sorting of material to form a distributable kit involves number of people, making of a school bag from re-usable material involves different set of people with requisite skills sets.In order to improve productivity, we will make an ERP which will predict sales of the NGO for every month Accurate sales forecasts will enable NGO to make informed business decisions and predict short-term and long-term performance Also in our ERP, we will allow to share weekly Report into the database as well as graphical representation of sales which will help to assess the performance"
-	},
+	
 
 ];
 export default cont
